acc13e17122fb8265b2649d2ed882f6ca0fb7a1a
Pham Quy <phamsyquybk@gmail.com>
2023-06-18 20:53:54 -0700

    firs change

---
+++ api/v1alpha1/dockercluster_types.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DockerClusterSpec defines the desired state of DockerCluster
+type DockerClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// ControlPlaneEndpoint represents the endpoint used to communicate with the control plane.
+	// +optional
+	ControlPlaneEndpoint clusterv1.APIEndpoint `json:"controlPlaneEndpoint"`
+
+	// LoadBalancerImage allows you override the load balancer image. If not specified a
+	// default image will be used.
+	// +optional
+	LoadBalancerImage string `json:"loadbalancerImage,omitempty"`
+}
+
+// DockerClusterStatus defines the observed state of DockerCluster
+type DockerClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Ready indicates that the cluster is ready.
+	// +optional
+	// +kubebuilder:default=falctrl.SetupSignalHandler()se
+	Ready bool `json:"ready"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// DockerCluster is the Schema for the dockerclusters API
+type DockerCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DockerClusterSpec   `json:"spec,omitempty"`
+	Status DockerClusterStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// DockerClusterList contains a list of DockerCluster
+type DockerClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DockerCluster `json:"items"`
+}
+
+const (
+	// ClusterFinalizer allows cleaning up resources associated with
+	// DockerCluster before removing it from the apiserver.
+	ClusterFinalizer = "dockercluster.infrastructure.cluster.x-k8s.io"
+)
+
+func init() {
+	SchemeBuilder.Register(&DockerCluster{}, &DockerClusterList{})
+}
---
+++ internal/controller/dockercluster_controller.go
@@ -0,0 +1,187 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/annotations"
+	"sigs.k8s.io/cluster-api/util/patch"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	infrav1 "github.com/capi-samples/cluster-api-provider-docker/api/v1alpha1"
+	"github.com/capi-samples/cluster-api-provider-docker/pkg/container"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	klog "k8s.io/klog/v2"
+
+	"github.com/capi-samples/cluster-api-provider-docker/pkg/docker"
+	"sigs.k8s.io/cluster-api/util"
+
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	"github.com/capi-samples/cluster-api-provider-docker/pkg/container"
+	"sigs.k8s.io/cluster-api/util/predicates"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+// DockerClusterReconciler reconciles a DockerCluster object
+type DockerClusterReconciler struct {
+	client.Client
+	Scheme           *runtime.Scheme
+	ContainerRuntime container.Runtime
+}
+
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=dockerclusters,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=dockerclusters/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=dockerclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the DockerCluster object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
+func (r *DockerClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (_ ctrl.Result, rerr error) {
+	logger := log.FromContext(ctx)
+
+	ctx = container.RuntimeInto(ctx, r.ContainerRuntime)
+
+	dockerCluster := &infrav1.DockerCluster{}
+	if err := r.Client.Get(ctx, req.NamespacedName, dockerCluster); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Get the Cluster
+	cluster, err := util.GetOwnerCluster(ctx, r.Client, dockerCluster.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if cluster == nil {
+		logger.Info("Waiting for Cluster Controller to set OwnerRef on DockerCluster")
+		return ctrl.Result{}, nil
+	}
+
+	logger = logger.WithValues("cluster", klog.KObj(cluster))
+	ctx = ctrl.LoggerInto(ctx, logger)
+
+	if annotations.IsPaused(cluster, dockerCluster) {
+		logger.Info("DockerCluster or owning Cluster is marked as paused, not reconciling")
+
+		return ctrl.Result{}, nil
+	}
+
+	// Create a helper for managing a docker container hosting the loadbalancer.
+	externalLoadBalancer, err := docker.NewLoadBalancer(ctx, cluster, dockerCluster)
+	if err != nil {
+		return ctrl.Result{}, apierrors.Wrapf(err, "failed to create helper for managing the externalLoadBalancer")
+	}
+
+	// Initialize the patch helper
+	patchHelper, err := patch.NewHelper(dockerCluster, r.Client)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	// Always attempt to Patch the DockerCluster object and status after each reconciliation.
+	defer func() {
+		if err := patchHelper.Patch(ctx, dockerCluster); err != nil {
+			logger.Error(err, "failed to patch DockerCluster")
+			if rerr == nil {
+				rerr = err
+			}
+		}
+	}()
+
+	// Handle deleted clusters
+	if !dockerCluster.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(ctx, dockerCluster, externalLoadBalancer)
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *DockerClusterReconciler) reconcileNormal(ctx context.Context, dockerCluster *infrav1.DockerCluster, externalLoadBalancer *docker.LoadBalancer) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+	logger.Info("Reconciling DockerCluster")
+
+	if !controllerutil.ContainsFinalizer(dockerCluster, infrav1.ClusterFinalizer) {
+		controllerutil.AddFinalizer(dockerCluster, infrav1.ClusterFinalizer)
+
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	// Create the docker container hosting the load balancer if it does not exist.
+	if err := externalLoadBalancer.Create(ctx); err != nil {
+		return ctrl.Result{}, apierrors.Wrap(err, "failed to create load balancer")
+	}
+
+	// Get the load balancer IP so we can use it for the enpoint address
+	lbIP, err := externalLoadBalancer.IP(ctx)
+	if err != nil {
+		return ctrl.Result{}, apierrors.Wrap(err, "failed to get IP for the load balancer")
+	}
+
+	dockerCluster.Spec.ControlPlaneEndpoint = clusterv1.APIEndpoint{
+		Host: lbIP,
+		Port: 6443,
+	}
+	dockerCluster.Status.Ready = true
+	return ctrl.Result{}, nil
+}
+
+func (r *DockerClusterReconciler) reconcileDelete(ctx context.Context, dockerCluster *infrav1.DockerCluster, externalLoadBalancer *docker.LoadBalancer) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+	logger.Info("Reconciling DockerCluster deletion")
+
+	// Delete the docker container hosting the load balancer
+	if err := externalLoadBalancer.Delete(ctx); err != nil {
+		return ctrl.Result{}, apierrors.Wrap(err, "failed to delete load balancer")
+	}
+	// Cluster is deleted so remove the finalizer.
+	controllerutil.RemoveFinalizer(dockerCluster, infrav1.ClusterFinalizer)
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *DockerClusterReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
+	c, err := ctrl.NewControllerManagedBy(mgr).
+		For(&infrav1.DockerCluster{}).
+		//WithOptions(options).
+		WithEventFilter(predicates.ResourceNotPaused(ctrl.LoggerFrom(ctx))).
+		Build(r)
+	if err != nil {
+		return err
+	}
+	return c.Watch(
+		&source.Kind{Type: &clusterv1.Cluster{}},
+		handler.EnqueueRequestsFromMapFunc(util.ClusterToInfrastructureMapFunc(ctx, infrav1.GroupVersion.WithKind("DockerCluster"), mgr.GetClient(), &infrav1.DockerCluster{})),
+		predicates.ClusterUnpaused(ctrl.LoggerFrom(ctx)),
+	)
+}
